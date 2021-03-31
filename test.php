249a7d9a80b39aa4d0ee18a339f3b6ce74c699e7
Jonas Osburg <jonas.osburg@eyeworkers.de>
2021-03-31 14:21:35 +0200

    Remove uneccessary code

--- test.php
+++ test.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace App\Entity;
-
 use Doctrine\ORM\Mapping as ORM;
 
 trait Sortable
@@ -10,16 +8,4 @@ trait Sortable
      * @ORM\Column(type="integer")
      */
     private $sort = 0;
-
-    public function getSort(): ?int
-    {
-        return $this->sort;
-    }
-
-    public function setSort(int $sort): self
-    {
-        $this->sort = $sort;
-
-        return $this;
-    }
 }
